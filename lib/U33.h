479d8f6e77768cd5d15836e693623494476c35f3
Ed FitzGerald <mecury421@gmail.com>
2012-09-23 11:35:58 +0100

    Add license and reformat

--- lib/U33.h
+++ lib/U33.h
@@ -1,14 +1,41 @@
 //------------------------------------------------------------------------------
 //
-// Name:    U33.h
-// Author:  Ed FitzGerald
+// Filename: U33.h
+// Author:   Ed FitzGerald
 //
+// Copyright (c) 2012 Ed FitzGerald
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //------------------------------------------------------------------------------
 
-#ifndef U33_H
-#define U33_H
+#ifndef OMATH_U33_H
+#define OMATH_U33_H
 
+//------------------------------------------------------------------------------
+// System Includes
+//------------------------------------------------------------------------------
 #include <iosfwd>
+
+//------------------------------------------------------------------------------
+// Library Includes
+//------------------------------------------------------------------------------
+// none
 
 //------------------------------------------------------------------------------
 //
@@ -81,4 +108,4 @@ private:
     /// @}
 };
 
-#endif // U33_H
+#endif // OMATH_U33_H
